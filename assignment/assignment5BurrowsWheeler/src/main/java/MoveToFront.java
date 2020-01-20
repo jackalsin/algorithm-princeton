adb983b0468219c3051c90024a38b46d72880251
jackalsin <jackal.sin@hotmail.com>
2020-01-20 19:51:06 +0800

    Add init

---
+++ assignment/assignment5BurrowsWheeler/src/main/java/MoveToFront.java
@@ -0,0 +1,20 @@
+/**
+ * @author jacka
+ * @version 1.0 on 1/20/2020
+ */
+public class MoveToFront {
+
+  // apply move-to-front encoding, reading from standard input and writing to standard output
+  public static void encode() {
+  }
+
+  // apply move-to-front decoding, reading from standard input and writing to standard output
+  public static void decode() {
+  }
+
+  // if args[0] is "-", apply move-to-front encoding
+  // if args[0] is "+", apply move-to-front decoding
+  public static void main(String[] args) {
+  }
+
+}
